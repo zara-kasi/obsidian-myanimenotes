720e83d8c517e820f354ea9dbd83e3539eab6772
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-10-05 20:59:40 +0530

    Create index.ts

---
+++ src/settings/index.ts
@@ -0,0 +1,5 @@
+// Settings module exports
+
+export { CassetteSettingTab } from './settings-tab';
+export { CassetteSettings, DEFAULT_SETTINGS } from './settings-interface';
+export type { MALUserInfo } from '../auth/mal';
