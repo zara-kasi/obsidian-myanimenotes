f4210936741527b8dac287c0c3009f763d9ea998
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-10-05 20:22:06 +0530

    Create constants.ts

---
+++ src/auth/mal/constants.ts
@@ -0,0 +1,9 @@
+// MAL API endpoints and configuration
+
+export const MAL_AUTH_URL = 'https://myanimelist.net/v1/oauth2/authorize';
+export const MAL_TOKEN_URL = 'https://myanimelist.net/v1/oauth2/token';
+export const MAL_USER_URL = 'https://api.myanimelist.net/v2/users/@me';
+export const REDIRECT_URI = 'obsidian://cassette-auth/mal';
+
+// Token expiry buffer (5 minutes in milliseconds)
+export const TOKEN_EXPIRY_BUFFER = 5 * 60 * 1000;
