57d8eb7d71290ce1c218b069b07b12ab32498e4a
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-10-06 15:30:57 +0530

    added simkl

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -4,3 +4,4 @@ export { CassetteSettingTab } from './settings-tab';
 export type { CassetteSettings } from './settings-interface';
 export { DEFAULT_SETTINGS } from './settings-interface';
 export type { MALUserInfo } from '../auth/mal';
+export type { SimklUserInfo } from '../auth/simkl';
