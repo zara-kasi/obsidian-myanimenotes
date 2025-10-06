83c4d27699ffa1ae6bbbfc7ba3cb74c184a4f1b5
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-10-06 16:11:40 +0530

    Register OAuth protocol handler for SIMKL

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@
 import { Plugin } from 'obsidian';
 import { CassetteSettingTab } from './settings/settings-tab';
 import { CassetteSettings, DEFAULT_SETTINGS } from './settings/settings-interface';
-import { handleOAuthRedirect } from './auth/mal';
+import { handleOAuthRedirect as handleMALRedirect } from './auth/mal';
+import { handleOAuthRedirect as handleSimklRedirect } from './auth/simkl';
 
 export default class CassettePlugin extends Plugin {
   settings: CassetteSettings;
@@ -19,7 +20,13 @@ export default class CassettePlugin extends Plugin {
     // Register OAuth protocol handler for MAL
     this.registerObsidianProtocolHandler('cassette-auth/mal', async (params) => {
       console.log('[Cassette] Received MAL OAuth callback');
-      await handleOAuthRedirect(this, params);
+      await handleMALRedirect(this, params);
+    });
+
+    // Register OAuth protocol handler for SIMKL
+    this.registerObsidianProtocolHandler('cassette-auth/simkl', async (params) => {
+      console.log('[Cassette] Received SIMKL OAuth callback');
+      await handleSimklRedirect(this, params);
     });
 
     console.log('[Cassette] Plugin loaded');
