b97bb5a9f61f13e7713d99f1b1dec86bc7f7bbbb
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-10-07 09:54:26 +0530

    Settings module exports with property customization

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,7 +1,6 @@
-// Settings module exports
-
 export { CassetteSettingTab } from './settings-tab';
 export type { CassetteSettings } from './settings-interface';
 export { DEFAULT_SETTINGS } from './settings-interface';
 export type { MALUserInfo } from '../auth/mal';
 export type { SimklUserInfo } from '../auth/simkl';
+export { renderPropertyMappingSection, renderPropertyOrderSection } from './property-settings';
