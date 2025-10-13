6723ea0310d51341f0f45f485677a3d7cf86809d
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-10-13 17:22:30 +0530

    update src/api/index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,7 @@
-export * from './providers/mal';
-export * from './providers/simkl';
+// Main API module exports
+
+// MAL API exports
+export * from './mal';
+
+// SIMKL API exports
+export * from './simkl';
