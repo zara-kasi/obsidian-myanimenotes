649db97462456f9ad43cfd3ed9c099fcbb891eca
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-10-13 17:28:15 +0530

    update src/settings/index.ts

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,6 +1,5 @@
 export { CassetteSettingTab } from './settings-tab';
 export type { CassetteSettings } from './settings-interface';
 export { DEFAULT_SETTINGS } from './settings-interface';
-export type { MALUserInfo } from '../auth/mal';
-export type { SimklUserInfo } from '../auth/simkl';
+export type { MALUserInfo, SimklUserInfo } from '../api';
 export { renderPropertyMappingSection } from './property-settings';
