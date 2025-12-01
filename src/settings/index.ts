916cde053b99528284f6d79e8bde7249fc6f5aef
Zarakasi <zarakasi.dev@gmail.com>
2025-12-01 12:26:17 +0530

    update src/settings/index.ts

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -3,3 +3,13 @@ export type { CassetteSettings } from './settings-interface';
 export { DEFAULT_SETTINGS } from './settings-interface';
 export type { MALUserInfo } from '../api/mal';
 export { renderPropertyMappingSection } from './property-settings';
+
+export { TemplateModal } from './template-modal';
+export type { TemplateConfig, PropertyItem, PropertyMetadata } from './template-config';
+export { 
+  DEFAULT_ANIME_TEMPLATE, 
+  DEFAULT_MANGA_TEMPLATE,
+  getPropertyMetadata,
+  getAvailableProperties,
+  generatePropertyId
+} from './template-config';
