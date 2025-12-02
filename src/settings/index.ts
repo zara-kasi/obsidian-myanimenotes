fddda74a643d72f8cc1c6ec23dd27ee3cdd09e3b
Zarakasi <zarakasi.dev@gmail.com>
2025-12-02 09:15:57 +0530

    Refactor settings module exports
    Removed property customization export and updated comments.

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,8 +1,13 @@
+/**
+ * Settings Module Exports
+ * 
+ * Property customization removed - template system only
+ */
+
 export { CassetteSettingTab } from './settings-tab';
 export type { CassetteSettings } from './settings-interface';
 export { DEFAULT_SETTINGS } from './settings-interface';
 export type { MALUserInfo } from '../api/mal';
-export { renderPropertyMappingSection } from './property-settings';
 
 // Template configuration exports
 export type { TemplateConfig, PropertyItem, PropertyMetadata } from './template-config';
