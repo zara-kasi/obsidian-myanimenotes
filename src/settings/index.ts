f5b3cdd8383fb4eea169d94b7f35ed8e242e8411
Zarakasi <zarakasi.dev@gmail.com>
2025-12-06 13:53:46 +0530

    update src/settings/index.ts

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,6 +1,6 @@
 export { CassetteSettingTab } from './settings-tab';
-export type { CassetteSettings, PropertyMapping } from './settings-interface';
-export { DEFAULT_SETTINGS, DEFAULT_PROPERTY_MAPPING } from './settings-interface';
+export type { CassetteSettings } from './settings-interface';
+export { DEFAULT_SETTINGS } from './settings-interface';
 export type { MALUserInfo } from '../api/mal';
 
 // Template configuration exports
