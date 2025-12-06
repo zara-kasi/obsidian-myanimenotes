9fbfe1cafc0bd699f53cc19d24e7e1bcb36c3f56
Zarakasi <zarakasi.dev@gmail.com>
2025-12-06 10:36:59 +0530

    update src/storage/markdown/index.ts

--- src/storage/markdown/index.ts
+++ src/storage/markdown/index.ts
@@ -8,9 +8,3 @@ export {
   generateFrontmatterProperties,
   updateMarkdownFileFrontmatter
 } from './markdown-generator';
-
-// Property mapping (internal use only)
-export type { PropertyMapping } from './property-mapping';
-export {
-  DEFAULT_PROPERTY_MAPPING
-} from './property-mapping';
