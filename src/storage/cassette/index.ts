4d1c6bba3d37da64fa8f68669832a3a51f3356df
Zarakasi <zarakasi.dev@gmail.com>
2025-12-10 11:49:10 +0530

    update src/storage/cassette/index.ts

--- src/storage/cassette/index.ts
+++ src/storage/cassette/index.ts
@@ -1,9 +1,9 @@
 /**
- * Cassette Module Exports
+ * MyAnimeNotes Module Exports
  * 
- * Exports cassette sync utilities, locking mechanisms, and index
+ * Exports myanimenotes sync utilities, locking mechanisms, and index
  */
 
-export * from './cassette-sync-manager';
-export { CassetteLockManager, createCassetteLockManager } from './cassette-lock';
-export * from './cassette-index';
+export * from './myanimenotes-sync-manager';
+export { MyAnimeNotesLockManager, createMyAnimeNotesLockManager } from './myanimenotes-lock';
+export * from './myanimenotes-index';
